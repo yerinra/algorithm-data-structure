91c06f96ece91690169d0a3146a05dd7ca7b2b58
yerinra <yerinaleph@gmail.com>
2023-11-30 18:32:23 +0900

    [#2]

---
+++ pass/1. 입출력과 사칙연산/q2557.js
@@ -0,0 +1,19 @@
+// Hello World!를 출력하시오.
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on("line", (line) => {
+  input = line.split(" ").map((el) => el);
+  rl.close();
+});
+
+rl.on("close", function () {
+  console.log("Hello World!");
+  process.exit();
+});
